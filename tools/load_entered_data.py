edc0c9b6cfe70f7f7e5f8e73b99c688d16b0d827
bendmorris <ben@bendmorris.com>
2012-06-11 13:01:30 -0600

    Adding ebird taxonomy file to load_entered_data.

--- tools/load_entered_data.py
+++ tools/load_entered_data.py
@@ -3,7 +3,7 @@ import tax_resolve
 
 species_lists = [
                  ('mammals', '../templates/sp_list_mammals.csv', [('../data/mammals.csv', 1, 3)], None),
-                 #('birds', '../templates/sp_list_birds.csv',     [('../data/birds.csv', 0, 0)], None),
+                 ('birds', '../templates/sp_list_birds.csv',     [('../data/ebird_tax_clean.csv', 0, 1)], None),
                  ('plants', '../templates/sp_list_plants.csv',   [('../data/plants.csv', 2, 3)], None),
                  ('inverts', '../templates/sp_list_inverts.csv', [('../data/beetles.csv', 1, 3), 
                                                                   ('../data/mosquitoes.csv', 1, 3)],
@@ -36,24 +36,33 @@ for taxon, data_entry_file, spp_code_files, synonyms_files in species_lists:
                 sci_name = cols[sciname_col]
                 spp_codes[sci_name] = spp_code
         data_file.close()
+    correct = 0
+    unknown = 0
+
     data_file = open(data_entry_file, 'r')
     data = data_file.read().replace('\r', '\n')
     lines = data.split('\n')[1:]
     for line in lines:
         line = line.strip()
         if line:
-            site,genus,sp,subsp,common_name,source = [s.strip() for s in line.split(',')]
-            if genus and sp:
-                sci_name = '%s %s' % (genus, sp)
-                if sci_name in spp_codes:
-                    print '%s: %s' % (sci_name, spp_codes[sci_name])
-                else:
-                    corrected_name = tax_resolve.tax_resolve(sci_name, com_name=common_name if common_name else None, 
-                                                             syns=syns)
-                    if corrected_name and corrected_name in spp_codes:
-                        print 'corrected: %s: %s' % (sci_name, spp_codes[corrected_name])
+            try:
+                site,genus,sp,subsp,common_name,source = [s.strip() for s in line.split(',')]
+                if genus and sp:
+                    sci_name = '%s %s' % (genus, sp)
+                    if sci_name in spp_codes:
+                        correct += 1
                     else:
-                        print '**%s' % sci_name
-            elif common_name:
-                corrected_name = tax_resolve.tax_resolve(com_name=common_name, syns=syns)
-                print '**%s' % common_name
+                        corrected_name = tax_resolve.tax_resolve(sci_name, com_name=common_name if common_name else None, 
+                                                                syns=syns)
+                        if corrected_name and corrected_name in spp_codes:
+                            print 'corrected: %s -> %s: %s' % (sci_name, corrected_name, spp_codes[corrected_name])
+                            correct += 1
+                        else:
+                            print '**%s' % sci_name
+                            unknown += 1
+                elif common_name:
+                    corrected_name = tax_resolve.tax_resolve(com_name=common_name, syns=syns)
+                    print '**%s' % common_name
+                    unknown += 1
+            except: print line
+    print '%s: Correct: %s; Unknown: %s (%s)' % (taxon, correct, unknown, correct / float(correct + unknown))
