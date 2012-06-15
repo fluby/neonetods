3c498238cc386be6650f3fc318d4c11cb9130a43
bendmorris <ben@bendmorris.com>
2012-06-15 13:09:18 -0600

    Added function to generate new spp_ids for species not present in taxonomy tables.

--- tools/load_entered_data.py
+++ tools/load_entered_data.py
@@ -2,10 +2,10 @@ import tax_resolve
 
 
 species_lists = [
-                 ('mammals', '../templates/sp_list_mammals.csv', [('../data/mammals.csv', 1, 3)], None),
-                 ('birds', '../templates/sp_list_birds.csv',     [('../data/ebird_tax_clean.csv', 0, 1)], None),
-                 ('plants', '../templates/sp_list_plants.csv',   [('../data/plants.csv', 2, 3)], None),
-                 ('inverts', '../templates/sp_list_inverts.csv', [('../data/beetles.csv', 1, 3), 
+                 ('mammals', '../data/sp_list_mammals.csv', [('../data/mammals.csv', 1, 3)], None),
+                 ('birds', '../data/sp_list_birds.csv',     [('../data/ebird_tax_clean.csv', 0, 1)], None),
+                 ('plants', '../data/sp_list_plants.csv',   [('../data/plants.csv', 2, 3)], None),
+                 ('inverts', '../data/sp_list_inverts.csv', [('../data/beetles.csv', 1, 3), 
                                                                   ('../data/mosquitoes.csv', 1, 3)],
                                                                  ['../data/mosquito_synonyms.csv']),
                  #herps
