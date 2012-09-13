03623c35ceff42a588422c1291ffd2a4e4c2dab9
bendmorris <ben@bendmorris.com>
2012-09-13 10:40:16 -0400

    Adding taxonomic groups to config.py for DRY.

--- tools/config.py
+++ tools/config.py
@@ -3,3 +3,5 @@ DATA_DIR = '/'.join(data.__file__.split('/')[:-1]) + '/'
 
 output_csv_args = ('email', 'password')
 connection_args = ('user', 'password', 'host', 'db')
+
+groups = ['mammals', 'birds', 'plants', 'inverts', 'herps']
