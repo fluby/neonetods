1e80cd59cb1063884b7dcaf1cb8a9e53262be258
jspencer88 <jspencer@neoninc.org>
2013-04-23 14:53:48 -0600

    file path to pkl file on local machin for sp list processing

--- tools_processing/config.py
+++ tools_processing/config.py
@@ -4,7 +4,7 @@ try:
     sys.path.append('/home/fsu/sp_list_processing')
     import data as data ##requires init file in directory
     DATA_DIR = '/'.join(data.__file__.split('/')[:-1]) + '/'
-except: DATA_DIR = K:\FSU1\Technician_Shared_Documents\Julia\dodobase\code\entered_data.pkl
+except: DATA_DIR = 'K:/FSU1/Informatics/splist_tax_git_to_db_local/entered_data.pkl'
 
 #Sentinelia log-in info
 mendeley_args = ('fsu.neoninc@gmail.com', 'insertpasswordhere')
