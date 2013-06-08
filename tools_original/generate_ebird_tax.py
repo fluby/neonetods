c8ebac3f680d88468f19ec6a31e47ab7fa88fc23
fluby <kate.thibault@yahoo.com>
2013-06-08 14:21:04 -0600

    updated ebird taxonomy and associated code

--- tools_original/generate_ebird_tax.py
+++ tools_original/generate_ebird_tax.py
@@ -3,18 +3,20 @@ import sys
 reload(sys)
 sys.setdefaultencoding('latin-1')
 
-tax_file = '../data/ebird-taxonomy-1-52_12-aug-2011.xls'
+tax_file = '../data/eBird-Clements-taxonomy-v1-53_21-Aug-2012.xls'
 output_file = '../data/ebird_tax_clean.csv'
 
 book = xlrd.open_workbook(tax_file)
 sheet = book.sheet_by_index(0)
 rows = sheet.nrows
-
+taxon_id = 'bird'
 output = open(output_file, 'w')
-output.write('spp_id,scientific_name,common_name')
+output.write('taxon_id, spp_id, source_id, scientific_name, common_name, order_name, family_name')
 for i in range(1, rows):
     row = sheet.row(i)
     if row:
-        _, sp_type, sci_name, comm_name, _, spp_id = [str(s.value) for s in row]
+        _, sp_type, spp_id, sci_name, comm_name, order1, family = [str(s.value) for s in row]
         if sp_type == 'species':
-            output.write('\n%s,%s,%s' % (spp_id, sci_name, comm_name))
+            out = '\nbird,%s,ebird,%s,%s,%s,%s' % (spp_id, sci_name, comm_name, order1, family)
+            output.write(out)
+output.close()
